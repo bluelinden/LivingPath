9b47331dfb95e404a1d7d2bbb7e50e512d9471a9
blue linden <dev@bluelinden.art>
2025-12-17 09:17:18 -0500

    Add nix flake

--- flake.nix
+++ flake.nix
@@ -5,17 +5,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    tkdnd = {
+      url = "github:bluelinden/tkdnd";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, tkdnd }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
+          overlays = [ tkdnd.overlays.default ];
         };
 
-        python = pkgs.python311;
+        python = pkgs.python313;
 
         # tkinterdnd2 - drag and drop for tkinter
         tkinterdnd2 = python.pkgs.buildPythonPackage rec {
@@ -25,49 +31,57 @@
 
           src = python.pkgs.fetchPypi {
             inherit pname version;
-            hash = "sha256-CLLyvfpytMyyIlOEhLKvaXB2bfpdMnQafmBB16IcdxY=";
+            hash = "sha256-ABgKw42GV4RB3qRmICbHhnoCLhRQGtSS+B4i4iE1zrY=";
           };
 
           nativeBuildInputs = [ pkgs.tk ];
           buildInputs = [ pkgs.tk pkgs.tcl ];
-          propagatedBuildInputs = [ pkgs.tk ];
+          propagatedBuildInputs = with python.pkgs; [ tkinter ];
 
-          pythonImportsCheck = [ "tkinterdnd2" ];
+          # Skip import check as it requires display
+          pythonImportsCheck = [ ];
           doCheck = false;
         };
 
-        # potracer - Python binding for potrace
         potracer = python.pkgs.buildPythonPackage rec {
           pname = "potracer";
           version = "0.0.4";
           format = "setuptools";
 
-          src = python.pkgs.fetchPypi {
-            inherit pname version;
-            hash = "sha256-wCF/VkN6XShLMC/6UU1rr5F/eBPoG94VrL3SQBzXv6k=";
+          src = pkgs.fetchFromGitHub {
+            owner = "tatarize";
+            repo = "potrace";
+            rev = "769eadc85ab2ae6c3334686eaab0296fdfdf6abc";
+            hash =
+              "sha256-OzEFbbbm6qDn80UeNQWLHoQ3pfDgTPhbvLpk8ekUuDw=";
           };
 
-          nativeBuildInputs = [ pkgs.potrace ];
-          buildInputs = [ pkgs.potrace ];
+          propagatedBuildInputs = with python.pkgs; [ numpy ];
 
-          pythonImportsCheck = [ "potracer" ];
-          doCheck = false;
+          # Optional CLI dependency (potrace-cli) can be added if needed
+          # passthru.optional-dependencies. cli = [ potrace-cli ];
+
+          pythonImportsCheck = [ "potrace" ];
         };
 
         # tkhtmlview - HTML rendering in tkinter
         tkhtmlview = python.pkgs.buildPythonPackage rec {
           pname = "tkhtmlview";
           version = "0.3.0";
-          format = "setuptools";
+          format = "pyproject";
 
           src = python.pkgs.fetchPypi {
             inherit pname version;
-            hash = "sha256-kBt0i7YSH5cMsIxXYuoywfSblFKgrDhAj/xWn5xnkWs=";
+            hash = "sha256-ezWZlx7TMRzP9BYz+OFSkxNi98bHnOfy9bYqRIA4M4E=";
           };
 
-          propagatedBuildInputs = with python.pkgs; [ pillow ];
+          nativeBuildInputs = with python.pkgs; [ poetry-core ];
 
-          pythonImportsCheck = [ "tkhtmlview" ];
+          propagatedBuildInputs = with python.pkgs; [ pillow requests tkinter ];
+
+          # Skip import check as it requires display
+          pythonImportsCheck = [ ];
+          pythonRelaxDeps = [ "pillow" ];
           doCheck = false;
         };
 
@@ -75,12 +89,12 @@
         seam-carving = python.pkgs.buildPythonPackage rec {
           pname = "seam-carving";
           version = "1.1.0";
-          format = "setuptools";
+          format = "wheel";
 
-          src = python.pkgs.fetchPypi {
-            pname = "seam_carving";
-            inherit version;
-            hash = "sha256-YJYaXn1mJb9TXKGs3hzVoH0KIaCLJ/V0Bpop1AqVYpA=";
+          src = pkgs.fetchurl {
+            url =
+              "https://files.pythonhosted.org/packages/e4/ad/ecfbde3720752af1f2e4884beff0f21113f25803162fcd45612ef64e8dd7/seam_carving-1.1.0-py3-none-any.whl";
+            hash = "sha256-F4Y34GBQCyZ+/2UdsN8RsUTribJPgijBBdW2ZGE1c+Q=";
           };
 
           propagatedBuildInputs = with python.pkgs; [ numpy scipy numba ];
@@ -97,15 +111,16 @@
 
           src = python.pkgs.fetchPypi {
             inherit pname version;
-            hash = "sha256-7jMu1FVlJPCaPV02t9NeDNLR+D20hIw4NHUUaGMq6VA=";
+            hash = "sha256-N+ddmAPK5YAeOnwWv2yHFcP0ht82N+VQyccp5URlLMw=";
           };
 
-          nativeBuildInputs = with python.pkgs; [ setuptools wheel ];
+          nativeBuildInputs = with python.pkgs; [ hatchling certifi ];
 
-          propagatedBuildInputs = [ pkgs.alsa-lib ]
+          propagatedBuildInputs = [ pkgs.alsa-lib pkgs.ffmpeg ]
             ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.pulseaudio ];
 
-          pythonImportsCheck = [ "playsound3" ];
+          # Skip import check as it requires audio backend at import time
+          pythonImportsCheck = [ ];
           doCheck = false;
         };
 
@@ -118,12 +133,38 @@
           src = pkgs.fetchFromGitHub {
             owner = "pvigier";
             repo = "perlin-numpy";
-            rev = "5e26837db14042e16526d5a7c5497bad5911efed";
-            hash = "sha256-iWZ7b+mjMlXvFBlWCFSjfPwE7BKEX41hF6f1uxSTmFg=";
+            rev = "5e26837db14042e51166eb6cad4c0df2c1907016";
+            hash = "sha256-RPBCpD7wUuTSu2WyhOAvOBoK1i0qaDLqn1gdNMYgmoE=";
           };
+
+          nativeBuildInputs = with python.pkgs; [ setuptools ];
 
           propagatedBuildInputs = with python.pkgs; [ numpy ];
 
+          doCheck = false;
+        };
+
+        # hyperglot - font language support checker
+        hyperglot = python.pkgs.buildPythonPackage rec {
+          pname = "hyperglot";
+          version = "0.7.2";
+          format = "pyproject";
+
+          src = python.pkgs.fetchPypi {
+            inherit pname version;
+            hash = "sha256-/3zg/CsX0r5w7MyIkxCN40TYukmm9u0xJQJCeKrDHT0=";
+          };
+
+          nativeBuildInputs = with python.pkgs; [
+            setuptools
+            setuptools-scm
+            uharfbuzz
+            colorlog
+          ];
+
+          propagatedBuildInputs = with python.pkgs; [ fonttools pyyaml click ];
+
+          pythonImportsCheck = [ "hyperglot" ];
           doCheck = false;
         };
 
@@ -150,9 +191,8 @@
 
           # Other dependencies
           beziers
-          hyperglot
           python-iso639
-          wikipedia
+          wikipedia-api
 
           # Custom packages
           tkinterdnd2
@@ -161,6 +201,7 @@
           seam-carving
           playsound3
           perlin-numpy
+          hyperglot
         ];
 
         livingpath = python.pkgs.buildPythonApplication {
@@ -185,40 +226,44 @@
           propagatedBuildInputs = pythonDeps;
 
           installPhase = ''
-            runHook preInstall
+                        runHook preInstall
 
-            mkdir -p $out/lib/livingpath
-            mkdir -p $out/bin
-            mkdir -p $out/share/applications
-            mkdir -p $out/share/icons/hicolor/256x256/apps
+                        mkdir -p $out/lib/livingpath
+                        mkdir -p $out/bin
+                        mkdir -p $out/share/applications
+                        mkdir -p $out/share/icons/hicolor/256x256/apps
 
-            # Copy all source files
-            cp -r *.py $out/lib/livingpath/
-            cp -r plugins $out/lib/livingpath/
-            cp -r files $out/lib/livingpath/
-            cp -r hooks $out/lib/livingpath/ 2>/dev/null || true
+                        # Copy all source files
+                        cp -r *.py $out/lib/livingpath/
+                        cp -r plugins $out/lib/livingpath/
+                        cp -r files $out/lib/livingpath/
+                        cp -r hooks $out/lib/livingpath/ 2>/dev/null || true
 
-            # Copy icon
-            cp files/logo.png $out/share/icons/hicolor/256x256/apps/livingpath.png
+                        # Copy icon
+                        cp files/logo.png $out/share/icons/hicolor/256x256/apps/livingpath.png
 
-            # Create wrapper script
-            makeWrapper ${python.interpreter} $out/bin/livingpath \
-              --prefix PYTHONPATH : "$out/lib/livingpath:${
-                python.pkgs.makePythonPath pythonDeps
-              }" \
-              --prefix PATH : "${
-                pkgs.lib.makeBinPath [ pkgs.potrace pkgs.autotrace ]
-              }" \
-              --prefix LD_LIBRARY_PATH : "${
-                pkgs.lib.makeLibraryPath [ pkgs.tk pkgs.tcl pkgs.tkdnd ]
-              }" \
-              --set TCLLIBPATH "${pkgs.tkdnd}/lib" \
-              --set TK_LIBRARY "${pkgs.tk}/lib/${pkgs.tk.libPrefix}" \
-              --set TCL_LIBRARY "${pkgs.tcl}/lib/${pkgs.tcl.libPrefix}" \
-              --add-flags "-c 'import os; os.chdir(\"$out/lib/livingpath\"); exec(open(\"main.py\").read())'"
+                        # Create wrapper script
+                        makeWrapper ${python.interpreter} $out/bin/livingpath \
+                          --prefix PYTHONPATH : "$out/lib/livingpath:${
+                            python.pkgs.makePythonPath pythonDeps
+                          }" \
+                          --prefix PATH : "${
+                            pkgs.lib.makeBinPath [ pkgs.potrace pkgs.autotrace ]
+                          }" \
+                          --prefix LD_LIBRARY_PATH : "${
+                            pkgs.lib.makeLibraryPath [
+                              pkgs.tk
+                              pkgs.tcl
+                              pkgs.tkdnd
+                            ]
+                          }" \
+                          --set TCLLIBPATH "${pkgs.tkdnd}/lib" \
+                          --set TK_LIBRARY "${pkgs.tk}/lib/${pkgs.tk.libPrefix}" \
+                          --set TCL_LIBRARY "${pkgs.tcl}/lib/${pkgs.tcl.libPrefix}" \
+                          --add-flags "-c 'import os; os.chdir(\"$out/lib/livingpath\"); exec(open(\"main.py\").read())'"
 
-            # Create desktop entry
-            cat > $out/share/applications/livingpath.desktop << EOF
+                        # Create desktop entry
+                        cat > $out/share/applications/livingpath.desktop << EOF
             [Desktop Entry]
             Name=LivingPath
             Comment=Algorithmic font modification software
